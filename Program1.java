7f489d87bbe5710e3371a6de43e034e80982fba9
UEM <moulidutta2003@gmail.com>
2025-03-20 14:35:10 +0530

    changes done in code

--- Program1.java
+++ Program1.java
@@ -1,9 +1,9 @@
 public class Program1{
 public static void main(String[] args){
 	int a= 10,b=20,temp;
-	a=b;
-	b=temp;
-	temp=a;
+	a=a+b;
+	b=a-b;
+	a=a-b;
 	System.out.println(a);
 	System.out.println(b);	 
 }}
